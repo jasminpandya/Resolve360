ccd04490bdad412d7bf2c6f8a577c6ba437f210f
chhavis334 <chhavis332@gmail.com>
2025-08-08 11:36:00 +0530

    Your commit message

--- agent/app.py
+++ agent/app.py
@@ -1,5 +1,14 @@
+<<<<<<< Updated upstream
 from flask import Flask, render_template, request, redirect, url_for, session
+=======
+from datetime import datetime, timezone
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+>>>>>>> Stashed changes
 from werkzeug.security import check_password_hash, generate_password_hash
+import boto3
+import yaml  
+
+
 
 import uuid
 import os
@@ -12,6 +21,7 @@ os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 # Temporary in-memory storage for demo
 complaints = {}
+
 
 @app.route('/')
 def home():
@@ -94,6 +104,65 @@ def track():
         return render_template('track.html', complaint=complaint, complaint_id=complaint_id)
     return render_template('track.html')
 
+<<<<<<< Updated upstream
 
+=======
+@app.route('/complaints')
+def get_complaints():
+    return jsonify([
+        {"id": 1, "title": "Network issue", "status": "New", "description": "Unable to connect to the internet.", "created_at": "2023-10-01","raised_by": "John Doe", "assigned_to_group": "Support Team"},
+        {"id": 2, "title": "Login problem", "status": "InProgress","description": "Unable to connect to the internet.", "created_at": "2023-10-01","raised_by": "John Doe", "assigned_to_group": "Support Team"},
+        {"id": 3, "title": "Payment failure", "status": "completed","description": "Unable to connect to the internet.", "created_at": "2023-10-01","raised_by": "John Doe", "assigned_to_group": "Support Team"},
+        {"id": 4, "title": "Billing", "status": "New","description": "Unable to connect to the internet.", "created_at": "2023-10-01","raised_by": "John Doe", "assigned_to_group": "Support Team"},
+        {"id": 5, "title": "Order placement", "status": "InProgress","description": "Unable to connect to the internet.", "created_at": "2023-10-01","raised_by": "John Doe", "assigned_to_group": "Support Team"},
+        {"id": 6, "title": "Payment stuck", "status": "completed","description": "Unable to connect to the internet.", "created_at": "2023-10-01","raised_by": "John Doe", "assigned_to_group": "Support Team"}
+    ])
+def read_yaml_file(file_path):
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
+
+@app.route('/api/complaint', methods=['POST'])
+def create_complaint_api():
+    data = request.form
+    name = data.get('name')
+    email = data.get('email')
+    phone = data.get('phone')
+    category = data.get('category')
+    description = data.get('description')
+
+    file = request.files.get('attachment')
+
+    try:
+       
+        config_path = os.path.join(os.path.dirname(__file__), 'prereqs/prereqs_config.yaml')
+        config = read_yaml_file(config_path)
+        kb_name = config['knowledge_base_name']
+
+     
+        dynamodb = boto3.resource('dynamodb')
+        smm_client = boto3.client('ssm')
+        table_name = smm_client.get_parameter(Name=f'{kb_name}-table-name', WithDecryption=False)
+        table = dynamodb.Table(table_name["Parameter"]["Value"])
+
+        complaint_id = str(uuid.uuid4())[:8]
+        table.put_item(
+            Item={
+                'complaint_id': complaint_id,
+                'name': name,
+                'email': email,
+                'phone': phone,
+                'category': category,
+                'description': description,
+                'status': 'open'
+            }
+        )
+
+        return jsonify({"status": "success", "complaint_id": complaint_id}), 200
+
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
+    
+    
+>>>>>>> Stashed changes
 if __name__ == '__main__':
     app.run(debug=True)
